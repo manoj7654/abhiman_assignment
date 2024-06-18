d0a0394f6566d2fb1ea1a6fdc9a16d2069794a90
Manoj Kumar <manojsfstm5@gmail.com>
2024-06-18 22:35:57 +0530

    register,login,createroom,joinroom frontend completed

---
+++ frontend/chatroom.js
@@ -0,0 +1,92 @@
+
+let createRoomForm = document.getElementById("createRoomForm");
+let joinRoomForm = document.getElementById("joinRoomForm");
+
+function toggleChatroomForm(formType) {
+ 
+    if (formType === 'create') {
+        createRoomForm.style.display = "block";
+        joinRoomForm.style.display = "none";
+    } else if (formType === 'join') {
+        createRoomForm.style.display = "none";
+        joinRoomForm.style.display = "block";
+    }
+}
+let token=localStorage.getItem("token")
+createRoomForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+
+    const roomData = {
+        roomId: document.getElementById('createroom').value,
+        password: document.getElementById('createpassword').value
+    };
+    console.log(roomData)
+    try {
+        const response = await fetch('http://localhost:4500/api/chatrooms', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(roomData)
+        });
+            console.log(response)
+        if (response.ok) {
+            const responseData = await response.json();
+            alert(responseData.message); // Show success message
+            // Redirect to the chatroom page
+            // window.location.href = 'chatroom.html';
+        } else {
+            const errorMessage = await response.text();
+            console.error('Error creating chatroom:', errorMessage);
+            // Display error message to the user
+            alert('Failed to create chatroom: ' + errorMessage);
+        }
+    } catch (error) {
+        console.error('Error creating chatroom:', error);
+        // Display error message to the user
+        alert('Failed to create chatroom: ' + error.message);
+    }
+});
+
+joinRoomForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    
+    // Get the values of roomId and roomPassword after the form submission
+    // let roomId = document.getElementById('roomId').value;
+    // let password = document.getElementById('password').value;
+    // console.log("password",password)
+    // Create the roomData object with the obtained values
+    const roomData = {
+        roomId: document.getElementById('roomId').value,
+        password: document.getElementById('password').value
+    };
+// console.log(token)
+    try {
+        const response = await fetch('http://localhost:4500/api/joinroom', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify(roomData)
+        });
+            console.log(response)
+        if (response.ok) {
+            const responseData = await response.json();
+            alert(responseData.message); // Show success message
+            // Redirect to the chatroom page
+            // window.location.href = 'chatroom.html';
+        } else {
+            const errorMessage = await response.text();
+            console.error('Error join chatroom:', errorMessage);
+            // Display error message to the user
+            alert('Failed to join chatroom: ' + errorMessage);
+        }
+    } catch (error) {
+        console.error('Error join chatroom:', error);
+        // Display error message to the user
+        alert('Failed to join chatroom: ' + error.message);
+    }
+});
---
+++ frontend/script.js
@@ -0,0 +1,99 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginToggle = document.getElementById('loginToggle');
+    const registerToggle = document.getElementById('registerToggle');
+    const loginForm = document.getElementById('loginForm');
+    const registerForm = document.getElementById('registerForm');
+
+    loginToggle.addEventListener('click', () => {
+        loginToggle.classList.add('active');
+        registerToggle.classList.remove('active');
+        loginForm.classList.add('active');
+        registerForm.classList.remove('active');
+    });
+
+    registerToggle.addEventListener('click', () => {
+        registerToggle.classList.add('active');
+        loginToggle.classList.remove('active');
+        registerForm.classList.add('active');
+        loginForm.classList.remove('active');
+    });
+
+    // user registeration
+    registerForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+             // Retrieve form data
+             const userId = document.getElementById('user').value;
+             const deviceId = document.getElementById('deviceId').value;
+             const name = document.getElementById('name').value;
+             const phone = document.getElementById('phone').value;
+             const password = document.getElementById('registerPassword').value;
+             const availableCoins = document.getElementById('availableCoins').value;
+             const isPrime = document.getElementById('isPrime').checked;
+     
+             // Create user data object
+             const userData = {
+                 userId: userId,
+                 deviceId: deviceId,
+                 name: name,
+                 phone: phone,
+                 password: password,
+                 availableCoins: availableCoins,
+                 isPrime: isPrime
+             };
+     
+        try {
+            const response = await fetch('http://localhost:4500/api/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData)
+            });
+           console.log(response)
+            if (response.ok) {
+                const data = await response.json();
+                // Handle successful registration
+                console.log(data)
+                alert(data.message)
+            } else {
+                // Handle registration error
+                const errorMessage = await response.text();
+                console.error('Registration failed:', errorMessage);
+            }
+        } catch (error) {
+            console.error('Error during registration:', error);
+        }
+    });
+
+    // user login
+    loginForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const userData = {
+            userId: document.getElementById('userId').value,
+            password: document.getElementById('loginPassword').value
+        };
+        try {
+            const response = await fetch('http://localhost:4500/api/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            });
+            if (response.ok) {
+               
+                const data = await response.json();
+                localStorage.setItem("token",data.token)
+                alert(data.message)
+                window.location.href = 'chatroom.html';
+            } else {
+                const errorMessage = await response.text();
+                console.error('Login failed:', errorMessage);
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+        }
+    });
+
+   
+});
